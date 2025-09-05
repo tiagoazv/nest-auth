80e68fb4fc256f4b075611dee7c83cfe2dacc43d
Tiago Azevedo <tgoncalvesazevedo@gmail.com>
2025-09-05 16:11:01 -0300

    feat: jwt authentication

---
+++ src/iam/authentication/authentication.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Body, HttpStatus, HttpCode } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+
+@Controller('authentication')
+export class AuthenticationController {
+    constructor(private readonly authenticationService: AuthenticationService) {}
+
+    @Post('sign-up')
+    async signUp(@Body() signUpDto: SignUpDto) {
+        await this.authenticationService.signUp(signUpDto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('sign-in')
+    async signIn(@Body() signInDto: SignInDto) {
+        await this.authenticationService.signIn(signInDto);
+    }
+}
